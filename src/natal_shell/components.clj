86494c93ca840abb06245d1276d5e043e46dde4c
Dan Motzenbecker <dan@oxism.com>
2015-11-15 12:57:58 -0500

    component macros

---
+++ src/natal_shell/components.clj
@@ -0,0 +1,20 @@
+(ns natal-shell.components
+  (:require
+    [natal-shell.components-list :refer [components]]
+    [natal-shell.utils :refer [to-kebab]]))
+
+
+(defn wrap-component [js-name]
+  `(defmacro ~(symbol (to-kebab js-name)) [props# & children#]
+    `(apply
+      js/React.createElement
+      ~'~(symbol (str "js/React." js-name))
+      (cljs.core/clj->js ~props#)
+      ~@children#)))
+
+
+(defmacro wrap-all []
+  `(do ~@(map wrap-component components)))
+
+
+(wrap-all)
