77a0a6f5116722d21a207650a738915d0a55567a
Dan Motzenbecker <dan@oxism.com>
2015-11-15 22:58:30 -0500

    project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject natal-shell "0.1.0"
+  :description "Simple wrapper around React Native API"
+  :url "https://github.com/dmotz/natal-shell/"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/mit-license.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [enlive "1.1.6"]])
