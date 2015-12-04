91b6d46ddea77c2e3f8e665f1c57297750206e37
Dan Motzenbecker <dan@oxism.com>
2015-12-04 00:26:58 -0500

    1.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject natal-shell "0.1.1"
+(defproject natal-shell "0.1.2"
   :description "A thin ClojureScript wrapper around the React Native API"
   :url "https://github.com/dmotz/natal-shell/"
   :license {:name "The MIT License (MIT)"
