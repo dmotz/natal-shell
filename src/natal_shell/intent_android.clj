b361fccfdb05c4b373aa320e09a6ffd5e6d9736d
Dan Motzenbecker <dan@oxism.com>
2015-12-20 11:37:10 -0500

    added intent_android

---
+++ src/natal_shell/intent_android.clj
@@ -0,0 +1,27 @@
+(ns
+ natal-shell.intent-android
+ "This namespace is automatically generated from the React Native docs.")
+(clojure.core/defmacro
+ open-url
+ [& args]
+ (clojure.core/seq
+  (clojure.core/concat
+   (clojure.core/list '.openURL)
+   (clojure.core/list 'js/React.IntentAndroid)
+   args)))
+(clojure.core/defmacro
+ can-open-url
+ [& args]
+ (clojure.core/seq
+  (clojure.core/concat
+   (clojure.core/list '.canOpenURL)
+   (clojure.core/list 'js/React.IntentAndroid)
+   args)))
+(clojure.core/defmacro
+ get-initial-url
+ [& args]
+ (clojure.core/seq
+  (clojure.core/concat
+   (clojure.core/list '.getInitialURL)
+   (clojure.core/list 'js/React.IntentAndroid)
+   args)))
