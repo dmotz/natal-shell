2d645f1648ea6d79aecb29b4bba963f7fa433dff
Dan Motzenbecker <dan@oxism.com>
2015-12-20 00:30:02 -0500

    added clone-with-rows macro

--- src/natal_shell/data_source.clj
+++ src/natal_shell/data_source.clj
@@ -2,3 +2,6 @@
 
 (defmacro data-source [config]
   `(js/React.ListView.DataSource. (cljs.core/clj->js ~config)))
+
+(defmacro clone-with-rows [ds rows]
+  `(.cloneWithRows ~ds (cljs.core/clj->js ~rows)))
