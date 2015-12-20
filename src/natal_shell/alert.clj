c24a830754d1c8813a08a10b7c63fd364ec94551
Dan Motzenbecker <dan@oxism.com>
2015-12-20 11:36:56 -0500

    added alert macros

---
+++ src/natal_shell/alert.clj
@@ -0,0 +1,11 @@
+(ns
+ natal-shell.alert
+ "This namespace is automatically generated from the React Native docs.")
+(clojure.core/defmacro
+ alert
+ [& args]
+ (clojure.core/seq
+  (clojure.core/concat
+   (clojure.core/list '.alert)
+   (clojure.core/list 'js/React.Alert)
+   args)))
