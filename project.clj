fd4018cc3662e883f29b419a5f4b782510b31684
Dan Motzenbecker <dan@oxism.com>
2016-04-17 15:52:08 -0400

    use clojure 1.8

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/dmotz/natal-shell/"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [enlive "1.1.6"]])
